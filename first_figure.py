a0d7c5ea77a4a2ca99e596b478ee81e1c85ce95c
James Coleman <jcolem22@msudenver.edu>
2020-05-05 10:37:09 -0600

    I set up my venv and wrote first_figure.py as begining exercise. job_candidates.cvs is the product of some repl practice I did with pandas and numpy.

---
+++ first_figure.py
@@ -0,0 +1,13 @@
+# Bokeh Libraries
+from bokeh.io import output_file
+from bokeh.plotting import figure, show
+
+# The figure wil be rendered into a static HTML file
+output_file('test_output_file.html',
+			title='Empty Bokeh Figure')
+
+# Set up a generic figure() object
+fig = figure()
+
+# view the figure
+show(fig)

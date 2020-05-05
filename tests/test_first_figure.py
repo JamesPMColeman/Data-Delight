e376e63c50b62ba11fb247d771f6f0f3fe31d565
James Coleman <jcolem22@msudenver.edu>
2020-05-05 13:37:21 -0600

    added a test file for glyph_glimpse

--- tests/test_first_figure.py
+++ tests/test_first_figure.py
@@ -4,7 +4,5 @@ from unittest.mock import patch
 
 class TestFirstFigure(unittest.TestCase):
 
-	@patch('scripts.first_figure.show')
-	@patch('scripts.first_figure.output_file')
-	def test_call_output_file(self, mock_show, mock_output):
+	def test_call_output_file(self):
 		pass
---
+++ tests/test_glyph_glimpse.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestGlyphGlimpse(unittest.TestCase):
+
+	def test_output_file_call(self):
+		pass

72b58293ea97325eb8e502b6d413746eccd23c85
James Coleman <jcolem22@msudenver.edu>
2020-05-06 15:18:37 -0600

    Used ColumnDataSources to create line graphs.

--- scripts/read_nba_data.py
+++ scripts/read_nba_data.py
@@ -1,5 +1,9 @@
+import os
+import sys
 import pandas as pd
 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Read NBA data from data file
 player_stats = pd.read_csv('data/2017-18_playerBoxScore.csv',
@@ -8,3 +12,10 @@ team_stats = pd.read_csv('data/2017-18_teamBoxScore.csv',
 						 parse_dates=['gmDate'])
 standings = pd.read_csv('data/2017-18_standings.csv',
 						parse_dates=['stDate'])
+
+# Western Confence
+west_top_2 = (standings[(standings['teamAbbr'] == 'HOU') |
+			 (standings['teamAbbr'] == 'GS')]
+			 .loc[:, ['stDate', 'teamAbbr', 'gameWon']]
+			 .sort_values(['teamAbbr', 'stDate']))
+
---
+++ scripts/western_conference.py
@@ -0,0 +1,42 @@
+from bokeh.io import output_file
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+
+
+from read_nba_data import *
+
+
+output_file('west_top_2_standings_race.html',
+			title='Western Conference Top 2 Teams Wins')
+
+# Isolate the Rockets and the Warriors
+rockets = west_top_2[west_top_2['teamAbbr'] == 'HOU']
+warriors = west_top_2[west_top_2['teamAbbr'] == 'GS']
+
+# Create ColumnDataSources
+rockets_column = ColumnDataSource(rockets)
+warriors_column = ColumnDataSource(warriors)
+
+# Create figure
+fig = figure(x_axis_type='datetime',
+			 plot_height=300,
+			 plot_width=600,
+			 title='Western Conference Top 2 Teams Wins',
+			 toolbar_location=None)
+
+# Render specs
+fig.step('stDate',
+		 'gameWon',
+		 color='#CE1141',
+		 legend_label='rockets',
+		 source=rockets_column)
+
+fig.step('stDate',
+		 'gameWon',
+		 color='#006BB6',
+		 legend_label='warriors',
+		 source=warriors_column)
+
+fig.legend.location = 'top_left'
+
+show(fig)
